c29adb0011a4075dd911a2828dcafc49e6ea38b3
ujeon jang <wj9304@gmail.com>
2023-10-22 15:30:52 +0900

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by 우전 on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
